19bbdd18d077cfbb9c9d4df48ea85b68f5478685
Warteruzannan S. Cunha <warteruzannan@gmail.com>
2022-12-06 00:49:03 -0300

    fix

--- src/main/java/br/com/advanse/eventsapi/application/configs/ApiConfigs.java
+++ src/main/java/br/com/advanse/eventsapi/application/configs/ApiConfigs.java
@@ -3,6 +3,4 @@ package br.com.advanse.eventsapi.application.configs;
 public class ApiConfigs {
     public static final String API_VERSION = "/api/v1/";
     public static final String API_EVENTS_PATH = API_VERSION + "events";
-
-    private  ApiConfigs(){}
 }
